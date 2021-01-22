953a83510298b2028ad1b9812a65a1a180c2815a
Brandon Ashby <bash7325@gmail.com>
2021-01-22 00:09:34 -0700

    small changes, psuedocode javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+//global Variables
+
+//Hide five day forecast on page load
+
+//Three API calls, append/text using currentCity ID's
+
+//API call for current searched city (UV in here?)
+
+//5 day forecast call
+
+//Check local storage, if has data restore it
+
+//Onclick to save to local storage
+})

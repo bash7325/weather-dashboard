0388ed6b5c0ae0d2603551919ad6c08f7a0afda4
Brandon Ashby <bash7325@gmail.com>
2021-01-23 08:59:23 -0700

    5 day and UV added

--- script.js
+++ script.js
@@ -7,7 +7,52 @@ var cities = [];
 //Three API calls, append/text using currentCity ID's
 
 //API call for current searched city (UV in here?)
+function currentCityForecast(city){
+    var APIKEY = "20d3501773a839af3857d2b0374101f6";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=Imperial&appid=" + APIKEY;
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        var weatherIcon = response.weather[0].icon;
+        var date = $("<h2>").text(moment().format('l'));
+        var icon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"); 
+        var temp = (response.main.temp)
+            
+        $("#currentCityName").text(response.name);
+        $("#currentCityName").append(date);
+        $("#currentCityName").append(icon);
+        $("#currentCityTemp").text(temp.toFixed(2) + " F");
+        $("#currentCityHumid").text(response.main.humidity + "%");
+        $("#currentCityWind").text(response.wind.speed + "MPH");
+
+            var lat = response.coord.lat
+            var lon = response.coord.lon
+            queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKEY + "&lat=" + lat + "&lon=" + lon; 
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response){
+                var uvIndex = response.value;
+                $("#currentCityUV").removeClass("favorable");
+                $("#currentCityUV").removeClass("moderate");
+                $("#currentCityUV").removeClass("severe");
+                    if (uvIndex <= 2.9){
+                        $("#currentCityUV").addClass("favorable");
+                    } else if (uvIndex >= 3 && uvIndex <= 7.9){
+                        $("#currentCityUV").addClass("moderate");
+                    } else {
+                        $("#currentCityUV").addClass("severe");
+                    };
+
+                    $("#currentCityUV").text(response.value);
+                
+            });   
+
+            $("#currentCity").show();   
+    }); 
+};
 //5 day forecast call
 function fiveDayForecast(city){
     var apiKey = "20d3501773a839af3857d2b0374101f6"
